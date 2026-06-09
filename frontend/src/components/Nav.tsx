3f799a14797f9e068859c311df47c55b88718828
Cao Hoang Phuc <caophuc@ubuntu-26.04-caophuc-computer>
2026-06-09 17:43:09 +0700

    style(ui): add class active for NavLink

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,11 +1,17 @@
 import { NavLink } from "react-router-dom";
 
 export default function Nav() {
+  const classActive = ({ isActive }: {isActive : boolean}) => 
+    `p-2 rounded-md ${
+    isActive ? "bg-blue-600 text-white font-bold" : "bg-blue-200 text-black font-bold"
+  }`;
+
+
   return (
-    <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/todo">Todo</NavLink>
-      <NavLink to="/about">About</NavLink>
+    <nav className="p-2 flex gap-2 justify-center">
+      <NavLink className={classActive} to="/">Home</NavLink>
+      <NavLink className={classActive} to="/todo">Todo</NavLink>
+      <NavLink className={classActive} to="/about">About</NavLink>
     </nav>
   )
 }
