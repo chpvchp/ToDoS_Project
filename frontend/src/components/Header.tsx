e41b6db28cca423866c46f0ea6b12f4062da831e
Cao Hoang Phuc <caophuc@ubuntu-26.04-caophuc-computer>
2026-06-09 17:32:10 +0700

    style(ui): add style for Header by Tailwind CSS

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
-    <header>
-      <h1>ToDoS Projects (CRUD)</h1>
+    <header className="p-2 flex justify-center items-center border-b border-gray-400">
+      <h1 className="p-2 font-bold text-lg">ToDoS Projects (CRUD)</h1>
     </header>
   )
 }
