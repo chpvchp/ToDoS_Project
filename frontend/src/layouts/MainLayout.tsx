d451b5fd4f029602952e42543bfcd55431bf27f0
Cao Hoang Phuc <caophuc@ubuntu-26.04-caophuc-computer>
2026-06-09 17:29:13 +0700

    feat(ui): add MainLayout

---
+++ frontend/src/layouts/MainLayout.tsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Nav from "../components/Nav";
+
+export default function MainLayout() {
+  return (
+    <>
+      <Header />
+      <Nav />
+
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  )
+}
