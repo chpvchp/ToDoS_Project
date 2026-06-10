a6cdc1a3e1c402dfe780623f27502a8d239d3ed3
Cao Hoang Phuc <caophuc@ubuntu-26.04-caophuc-computer>
2026-06-10 12:58:15 +0700

    feat(cardinput): add value, onClick and onChange prop

--- frontend/src/components/CardInput.tsx
+++ frontend/src/components/CardInput.tsx
@@ -1,19 +1,25 @@
 import Button from "./Button"
 
 type CardInputProps = {
-  label: string
+  label: string;
+  value: string;
+  onClick: () => void;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function CardInput({ label } : CardInputProps) {
+export default function CardInput({ label, value, onClick, onChange } : CardInputProps) {
   return (
     <div className="p-2 bg-white rounded-md border border-gray-400 flex gap-2">
       <input
         className="p-2 border border-gray-300 rounded-md transition duration-200 hover:border-gray-400"
         type="text"
+        value={value}
         placeholder="Input Title Todo..."
+        onChange={onChange}
       />
       <Button
         label={label}
+        onClick={onClick}
       />
     </div>
   )
