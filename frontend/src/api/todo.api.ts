b726fa7c2cbb700e525c744f96dcbdb7b4890842
Cao Hoang Phuc <caophuc@ubuntu-26.04-caophuc-computer>
2026-06-10 12:20:09 +0700

    feat(api): add todo.api.ts to fetch CRUD backend

---
+++ frontend/src/api/todo.api.ts
@@ -0,0 +1,22 @@
+import axios from "axios"
+
+const BASE_URL = "http://localhost:8000"
+
+export const getTodos = async () => {
+  const res = await axios.get(`${BASE_URL}/todos`);
+  return res.data;
+};
+
+export const postTodo = async (title: string) => {
+  const res = await axios.post(`${BASE_URL}/todos`, { title: title });
+  return res.data
+}
+
+export const deleteTodo = async (id: number) => {
+  await axios.delete(`${BASE_URL}/todos/${id}`);
+};
+
+export const putTodo = async (id: number, title: string) => {
+  const res = await axios.put(`${BASE_URL}/todos/${id}`, { title: title});
+  return res.data
+}
