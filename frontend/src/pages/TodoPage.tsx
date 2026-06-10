c4494d9c56e1f132fa364710951fe38c1e5cb3c3
Cao Hoang Phuc <caophuc@ubuntu-26.04-caophuc-computer>
2026-06-10 13:19:20 +0700

    feat: complete TodoPage - add hook, useState - add prop for CardInput and CardTodo

--- frontend/src/pages/TodoPage.tsx
+++ frontend/src/pages/TodoPage.tsx
@@ -1,20 +1,42 @@
+import { useState } from "react";
 import CardInput from "../components/CardInput";
 import CardTodo from "../components/CardTodo";
+import { useTodo } from "../hook/useTodo";
 
 export default function TodoPage() {
+  const [title, setTitle] = useState("");
+  const {
+    todos,
+    getListTodo,
+    createTodo,
+    removeTodo,
+    editTodo,
+    refetch
+  } = useTodo();
+
   return (
     <main className="m-2 pt-4 rounded-lg border border-gray-200 bg-gray-50 flex-1 flex flex-col gap-2 items-center">
       <CardInput
         label="Add"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        onClick={() => {
+          createTodo(title);
+          setTitle("");
+        }}
       />
 
       <p className="mt-2 italic">--- List Todo ---</p>
 
-      <div>
-        <CardTodo
-          title="React"
-        />
-
+      <div className="flex gap-2">
+        {todos.map((todo) => (
+          <CardTodo
+            key={todo.id}
+            title={todo.title}
+            onDel={() => removeTodo(todo.id)}
+            onSave={(newTitle) => editTodo(todo.id, newTitle)}
+          />
+        ))}
 
       </div>
 
