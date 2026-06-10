7e345e932d38bcb493422d8cce2c8aa67e03fa2c
Cao Hoang Phuc <caophuc@ubuntu-26.04-caophuc-computer>
2026-06-10 12:44:21 +0700

    feat(button): add onClick prop

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,11 +1,13 @@
 type ButtonProps = {
-  label: string
+  label: string;
+  onClick: () => void;
 }
 
-export default function Button({ label }: ButtonProps) {
+export default function Button({ label, onClick }: ButtonProps) {
   return (
     <button
       className="p-2 bg-blue-600 text-white rounded-md font-bold transition duration-200 hover:bg-blue-800"
+      onClick={onClick}
     >
       {label}
     </button>
