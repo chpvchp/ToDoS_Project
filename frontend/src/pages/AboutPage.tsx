3dabf7ef6dcc1915bbff486e5b560f2ba3fed751
Cao Hoang Phuc <caophuc@ubuntu-26.04-caophuc-computer>
2026-06-10 11:56:27 +0700

    chore(ui): add tag main for AboutPage

--- frontend/src/pages/AboutPage.tsx
+++ frontend/src/pages/AboutPage.tsx
@@ -1,7 +1,7 @@
 export default function AboutPage() {
   return (
-    <>
-      <p>This is Home Page</p>
-    </>
+    <main className="m-2 rounded-lg border border-gray-200 bg-gray-50 flex-1 flex flex-col gap-2 justify-center items-center">
+      
+    </main>
   )
 }
